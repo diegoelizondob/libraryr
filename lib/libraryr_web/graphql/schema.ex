329b4d42e0b52e79ce15ba1be11a71fc1751892e
luisangelescom <luis20.lgi@gmail.com>
2024-01-23 13:04:13 -0600

    Se agrega publish cuando se agrega un lector

--- lib/libraryr_web/graphql/schema.ex
+++ lib/libraryr_web/graphql/schema.ex
@@ -5,74 +5,75 @@ defmodule LibraryWeb.GraphQL.Schema do
   use Absinthe.Schema
   alias Library.Resolvers.CategoryResolver
 
-  import_types Absinthe.Type.Custom
+  import_types(Absinthe.Type.Custom)
 
   query do
     @desc "Get authors list"
     field :author, list_of(:author) do
-      resolve &CategoryResolver.list_authors/3
+      resolve(&CategoryResolver.list_authors/3)
     end
 
     @desc "Get books list"
     field :book, list_of(:books) do
-      resolve &CategoryResolver.list_books/3
+      resolve(&CategoryResolver.list_books/3)
     end
 
     @desc "Get one book by isbn"
     field :one_book, :books do
-      arg :isbn, non_null(:id)
-      resolve &CategoryResolver.get_book_by_isbn/3
+      arg(:isbn, non_null(:id))
+      resolve(&CategoryResolver.get_book_by_isbn/3)
     end
 
     @desc "Get the current user"
     field :category, list_of(:category) do
-      resolve &CategoryResolver.list_categories/3
+      resolve(&CategoryResolver.list_categories/3)
     end
 
     # - Readers queries:
 
     @desc "Get list of readers"
     field :readers, list_of(:reader) do
-      resolve &CategoryResolver.list_readers/3
+      resolve(&CategoryResolver.list_readers/3)
     end
 
     @desc "Get one reader by id"
     field :one_reader, :reader do
-      arg :id, non_null(:id)
-      resolve &CategoryResolver.get_reader_by_name/3
+      arg(:id, non_null(:id))
+      resolve(&CategoryResolver.get_reader_by_name/3)
     end
+
     # ------------
   end
 
   mutation do
     @desc "Create a book with authors"
     field :create_book_with_authors, :books do
-      arg :isbn, non_null(:string)
-      arg :title, non_null(:string)
-      arg :authors, list_of(:author_input)
-      arg :category_id, :integer
-      resolve &CategoryResolver.create_book_with_authors/3
+      arg(:isbn, non_null(:string))
+      arg(:title, non_null(:string))
+      arg(:authors, list_of(:author_input))
+      arg(:category_id, :integer)
+      resolve(&CategoryResolver.create_book_with_authors/3)
     end
 
     @desc "delete a book with authors"
     field :delete_book_with_authors, type: non_null(:string) do
-      arg :isbn, non_null(:string)
-      resolve &CategoryResolver.delete_book_with_authors/3
+      arg(:isbn, non_null(:string))
+      resolve(&CategoryResolver.delete_book_with_authors/3)
     end
 
     # - Readers mutations:
     @desc "Create a reader"
     field :create_reader, type: :reader do
-      arg :name, :string
-      arg :email, :string
-      arg :isbn, :string
-      resolve &CategoryResolver.create_reader/3
+      arg(:name, :string)
+      arg(:email, :string)
+      arg(:isbn, :string)
+      resolve(&CategoryResolver.create_reader/3)
     end
 
     @desc "Delete one reader"
     field :delete_reader, type: :delete_reader_obj do
-      arg :isbn, non_null(:string)
-      resolve &CategoryResolver.delete_reader_by_id/3
+      arg(:isbn, non_null(:string))
+      resolve(&CategoryResolver.delete_reader_by_id/3)
     end
 
     # ----------
@@ -81,10 +82,27 @@ defmodule LibraryWeb.GraphQL.Schema do
   subscription do
     @desc "subscribe to changes to  authors"
     field :authors_change, list_of(:author) do
-      config fn _args, _res ->
-        IO.puts "were here!!!!"
+      config(fn _args, _res ->
+        IO.puts("were here!!!!")
         {:ok, topic: :authors_change}
-      end
+      end)
+    end
+
+    @desc "subscribe to changes when adding an author"
+    field :book_reader, :reader do
+      config(fn _args, _res ->
+        IO.puts("were here!!!!")
+        {:ok, topic: "/1"}
+      end)
+
+      trigger(:create_reader,
+        topic: fn comment ->
+          IO.inspect("comment $$$$$$$$$$$")
+          IO.inspect(comment)
+          "/1"
+          # "testurl"
+        end
+      )
     end
   end
 
@@ -107,12 +125,12 @@ defmodule LibraryWeb.GraphQL.Schema do
     field :updated_at, :datetime
   end
 
-  #query do
+  # query do
   #  @desc "Get the current user"
   #  field :category, list_of(:category) do
   #    resolve &CategoryResolver.list_categories/3
   #  end
-  #end
+  # end
 
   object :category do
     field :id, non_null(:id)
